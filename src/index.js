c13e12bf81125a561b582f02c88d0d38137138ca
Abanoub Amir <abanwbamyr049@gmail.com>
2026-04-07 18:15:54 +0200

    add cors

--- src/index.js
+++ src/index.js
@@ -7,11 +7,18 @@ const sequelize = require('./db');
 
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -29,7 +36,6 @@ app.get('/api/db-check', async (req, res) => {
 });
 
 app.use('/', routes);
-
 app.use(errorMiddleware);
 
 module.exports = app;
