11968f879d330652c768c96c94c9cc72149ab148
Abanoub Amir <abanwbamyr049@gmail.com>
2026-04-14 17:39:37 +0200

    add admin

--- src/middlewares/roleSecurity.js
+++ src/middlewares/roleSecurity.js
@@ -2,11 +2,9 @@ const requireAdmin = (req, res, next) => {
     if (req.headers['x-user-role'] === 'admin') {
         return next();
     }
-    
-    // We can also route this to the global error handler 
-    // but a direct JSON response is very fast for auth middleware
-    return res.status(403).json({ 
-        success: false, 
+
+    return res.status(403).json({
+        success: false,
         message: "Forbidden: Admin access required.",
         errorCode: "FORBIDDEN"
     });
